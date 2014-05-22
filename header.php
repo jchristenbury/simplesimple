a5594c03c333b51ad282c3f048faf3f098c28568
John Christenbury <john@johnchristenburycreative.com>
2014-05-22 11:33:50 -0400

    Rewrote to include doctype; clean up code a bit;

--- header.php
+++ header.php
@@ -1,10 +1,11 @@
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width">
-	<title><?php wp_title(); ?></title>
-	<?php wp_head(); ?>
+	<title><?php if ( is_single() || is_page() || is_archive() ) {?> <?php wp_title(); ?> | <?php } bloginfo('name');?></title>
 	<!-- add favicon --> 
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
