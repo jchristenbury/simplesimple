9d7605e92eb5945933002f20cf2bd0c01fbc5768
John Christenbury <john@johnchristenburycreative.com>
2014-05-22 11:34:54 -0400

    Cut back code to only using a simple loop and output. Almost no markup currently.

--- index.php
+++ index.php
@@ -7,21 +7,20 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
-		<?php if ( have_posts() ) : ?>
-
-			<?php /* The loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'content', get_post_format() ); ?>
-			<?php endwhile; ?>
-
-			<?php simplesimple_paging_nav(); ?>
-
-		<?php else : ?>
-			<?php get_template_part( 'content', 'none' ); ?>
+		
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		
+			<h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+			<?php the_content(); ?>
+			
+		
+		
+			<?php endwhile; else: ?>
+			<p>Error</p>
 		<?php endif; ?>
+	
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
+<!--  sidebar *would* go here  -->
 <?php get_footer(); ?>
